e48ea077342d8aed342098fa18517cdf794f3d15
InnocentShiva <singh.akshat.solanki@gmail.com>
2025-05-16 19:05:49 +0530

    For sample docker-compose multiple container creation

---
+++ docker/learn-docker/Akshat_work_Docker/python-project-sample/app.py
@@ -0,0 +1,6 @@
+create docker file having output as - "Hello from my Python App"
+also print file of python will be having the detail - "Hey Deploy myy APP"
+
+
+
+Once done lets submit in on next class
