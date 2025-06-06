04bf1f3113272d4b00110adbb454746570adf64f
Ubuntu <ubuntu@ip-172-31-11-143.ap-south-1.compute.internal>
2025-06-06 14:27:57 +0000

    Python sample project file

--- docker/python-app/app.py
+++ docker/python-app/app.py
@@ -1,2 +1,21 @@
-print("Hello from my Python App")
-print("Hey Deploy myy APP")
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({
+        "message": "Hello, world! Your server is running.",
+        "status": "success"
+    })
+
+@app.route('/health')
+def health_check():
+    return jsonify({
+        "status": "ok",
+        "uptime": "running"
+    })
+
+if __name__ == '__main__':
+    # Run the Flask app on port 5000
+    app.run(host='0.0.0.0', port=8083)
